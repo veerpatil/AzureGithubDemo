b8e2a0a06d93d09da8bd10a708311178c30a8672
veerpatil <patil.veerkumar@outlook.com>
2022-07-28 20:08:05 +0530

    Updated to include docker compose yml

--- src/test/mytest/MySampleTest.java
+++ src/test/mytest/MySampleTest.java
@@ -3,12 +3,20 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import io.github.bonigarcia.wdm.config.DriverManagerType;
 import main.java.ExtentReportInit;
 import main.java.ExtentTestManager;
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.*;
 import test.mytest.ExtentReportListener;
+
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
 
 public class MySampleTest {
 
@@ -16,8 +24,7 @@ public class MySampleTest {
     WebDriver driver ;
     @BeforeClass()
     @Parameters({"browser"})
-    public void browserInit(String browser)
-    {
+    public void browserInit(String browser) throws MalformedURLException, URISyntaxException {
         if(browser.equals("Chrome")) {
             DriverManagerType chrome = DriverManagerType.CHROME;
             ChromeOptions options = new ChromeOptions();
@@ -33,6 +40,25 @@ public class MySampleTest {
             DriverManagerType firefox = DriverManagerType.FIREFOX;
             WebDriverManager.getInstance(firefox).setup();
             driver = new FirefoxDriver();
+        }
+        else if(browser.equals("Docker"))
+        {
+//            ChromeOptions options = new ChromeOptions();
+//            options.addArguments("--disable-gpu");
+//            options.addArguments("--disable-dev-shm-usage");
+//            options.addArguments("--no-sandbox");
+//            options.addArguments("--headless");
+//            DesiredCapabilities capability = DesiredCapabilities.ch;
+//            capability.setPlatform(Platform.LINUX);
+            DesiredCapabilities caps = new DesiredCapabilities();
+            caps.setBrowserName("chrome");
+//            caps.setVersion("103.0");
+            caps.setPlatform(Platform.LINUX);
+//            caps.setCapability(ChromeOptions.CAPABILITY,options);
+
+           driver = new RemoteWebDriver(URI.create("http://localhost:4444/").toURL(), caps);
+           driver.get("http://www.google.com");
+//            driver.quit();
         }
     }
 
